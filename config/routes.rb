cb4c7ff3b3ece4e6636f6319beb69c48478f57f4
alanyeh20001 <alanyeh20001@hotmail.com>
2017-11-15 17:59:54 +0800

    Add namespace 'api' & 'v1'

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root to: "authors#index"
 
-  resources :authors do
-    resources :books
+  namespace :api do
+    namespace :v1 do
+      resources :authors do
+        resources :books
+      end
+    end
   end
 end
