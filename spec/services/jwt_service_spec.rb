d4ff49f95e2712e1f269751eba163fedb7863752
alanyeh20001 <alanyeh20001@hotmail.com>
2017-11-16 10:01:31 +0800

    Add JWT service spec

---
+++ spec/services/jwt_service_spec.rb
@@ -0,0 +1,16 @@
+require "rails_helper"
+
+RSpec.describe JwtService do
+  subject { described_class }
+
+  let(:payload) { { 'one' => 'two' } }
+  let(:token) { 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJvbmUiOiJ0d28ifQ.pNUjHGirRVVXmcadRBQvV-uqzspENH5jqKaYk_JNQX8' }
+
+  describe '.encode' do
+    it { expect(subject.encode(payload)).to eq(token) }
+  end
+
+  describe '.decode' do
+    it { expect(subject.decode(token)).to eq(payload) }
+  end
+end
